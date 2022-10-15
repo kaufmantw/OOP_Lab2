25927e4449c7d58f2c29d36f63ed3466fe78b32b
kaufmantw <kaufmantw@jacks.sfasu.edu>
2022-10-15 13:10:20 -0500

    initial
    downloaded and added files to lab

---
+++ App2.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ * Read lines from the user's keyboard.  For each line, attempt to interpret
+ * it as a mathematical expression and print out the result (or an error message).
+ * 
+ * Lines are read as long as the user has not requested that the program be quit
+ *
+ */
+
+public class Driver {
+
+    /**
+     * @param args Not used
+     * @throws IOException If there is an IO error
+     */
+    public static void main(String[] args) throws IOException {
+
+        // Open standard in for input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // Loop until quit
+        while(true)
+        {
+            // Read the next line of input
+            String input = br.readLine();
+
+            // Parse, compute and report the result
+            if(Calculator.parseAndCompute(input)) 
+            {
+                // Quit has been specified
+                break;
+            }
+
+        }
+
+        // Close out the buffered reader
+        br.close();
+    }
+}
---
+++ CalculatorException.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * Lab 5
+ * 
+ * Base exception class for representing exceptions that can
+ * occur within a calculator.  
+ * 
+ * This class extends Exception, requiring only that message be given
+ * to determine the issue.
+ */
+public class CalculatorException extends Exception
+{
+}
